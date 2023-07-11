d3a3d072aa7670c4b382f98829fdd21b0d9d1372
agigibairene <i.agigiba@alustudent.com>
2023-07-11 08:57:31 +0000

    Recurse it

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""a recursive function that queries the Reddit
+ API and returns a list containing the titles of 
+all hot articles for a given subreddit."""
+
 
 def recurse(subreddit, hot_list=[], after=None, count=0):
     """
