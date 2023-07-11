d689a8b050fcbf3fd4562d0cd8bb30deb710479a
agigibairene <i.agigiba@alustudent.com>
2023-07-11 19:21:37 +0000

    api advanced

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -4,68 +4,56 @@ parses the title of all hot, and prints a sorted count
 of given keywords (case-insensitive, delimited by spaces"""
 
 
-def count_words(subreddit, word_list, after=None, count={}):
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Prints counts of given words found in hot posts of a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
     """
-    queries the Reddit API
-    parses title of all hot articles
-    prints sorted count of given keywords
-    """
-    import json
-    import requests
-    if after is None:
-        sub_URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    else:
-        sub_URL = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
-            subreddit, after)
-    subreddit_info = requests.get(sub_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False)
-    for word in word_list:
-        word = word.lower()
-        if word not in count.keys():
-            count[word] = 0
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     try:
-        data = subreddit_info.json().get("data")
-    except:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
         return
-    children = data.get("children")
-    for child in children:
-        title = (child.get("data").get("title").lower())
-        title = title.split(' ')
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
         for word in word_list:
-            word = word.lower()
-            count[word] += title.count(word)
-    after = data.get("after")
-    if after is None:
-        result = []
-        for k in count.keys():
-            if count[k] != 0:
-                if result == []:
-                    result.append("{}: {}".format(k, count[k]))
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
                 else:
-                    for i in range(len(result)):
-                        if count[k] > int(result[i].split(' ')[1]):
-                            result = result[:i] + \
-                                     ["{}: {}".format(k, count[k])] + \
-                                     result[i:]
-                            break
-                        elif count[k] == int(result[i].split(' ')[1]):
-                            alpha_list = [k, result[i].split(' ')[0]]
-                            j = 1
-                            while count[k] == int(result[i + j].split(' ')[1]):
-                                alpha_list.append(result[i + j].split(' ')[0])
-                            alpha_list = alpha_list.sort
-                            for j in range(len(alpha_list)):
-                                if k == alpha_list[j]:
-                                    result = result[:i + j] + \
-                                             ["{}: {}".format(k, count[k])] + \
-                                             result[i + j:]
-                        else:
-                            continue
-                    else:
-                        result.append("{}: {}".format(k, count[k]))
-        if result != []:
-            for printing in result:
-                print(printing)
-        return
-    return (count_words(subreddit, word_list, after, count))
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
